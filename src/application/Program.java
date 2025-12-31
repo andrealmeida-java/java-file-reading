396ecb14fe193e3c636a90b3e46942c35325abfb
André <andrealmeidadeveloper@gmail.com>
2025-12-31 11:22:37 -0300

    Otimizando Program com try-with-resources

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,12 +10,8 @@ public class Program {
 		// Reads a text file line by line and prints its content
 
 		String path = "in.txt";
-		FileReader fr = null;
-		BufferedReader br = null;
 		
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr);
+		try(BufferedReader br = new BufferedReader(new FileReader(path))) {
 			
 			String line = br.readLine();
 			
@@ -26,19 +22,6 @@ public class Program {
 		}
 		catch(IOException e){
 			System.out.println("Error: " + e.getMessage());
-		}
-		finally {
-			try {
-				if(br != null) {
-					br.close();
-				}
-				if(fr != null) {
-					fr.close();
-				}
-			}
-			catch(IOException e) {
-				e.printStackTrace();
-			}
 		}
 		
 	}
