70682719e4b647e6083ef814bd62a1f22f0ef1eb
André <andrealmeidadeveloper@gmail.com>
2025-12-31 11:12:07 -0300

    modificação do Program para usar as streams FileReader e BufferedReader

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,27 +1,43 @@
 package application;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
+		// Reads a text file line by line and prints its content
+
+		String path = "in.txt";
+		FileReader fr = null;
+		BufferedReader br = null;
 		
-		File file = new File("in.txt");
-		Scanner sc = null;
 		try {
-			sc = new Scanner(file);
-			while(sc.hasNextLine()) {
-				System.out.println(sc.nextLine());
+			fr = new FileReader(path);
+			br = new BufferedReader(fr);
+			
+			String line = br.readLine();
+			
+			while(line != null) {
+				System.out.println(line);
+				line = br.readLine();
 			}
 		}
-		catch(IOException e) {
+		catch(IOException e){
 			System.out.println("Error: " + e.getMessage());
 		}
 		finally {
-			if(sc != null) {
-				sc.close();
+			try {
+				if(br != null) {
+					br.close();
+				}
+				if(fr != null) {
+					fr.close();
+				}
+			}
+			catch(IOException e) {
+				e.printStackTrace();
 			}
 		}
 		
